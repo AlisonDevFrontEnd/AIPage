642e641081ec5747cf4903507415bdfa594c3108
AlisonDevFrontEnd <150954455+AlisonDevFrontEnd@users.noreply.github.com>
2024-08-16 18:16:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Create the "scroll to top" link once
+const toTopLink = document.createElement('a');
+toTopLink.setAttribute('href', '#to-top');
+toTopLink.style.position = 'fixed';
+toTopLink.style.bottom = '20px';
+toTopLink.style.right = '20px';
+toTopLink.style.display = 'none'; // Hide initially
+
+const toTopArrowImage = document.createElement('img');
+toTopArrowImage.setAttribute('src', 'img/scroll_to_top.svg');
+toTopArrowImage.setAttribute('style', 'width: 50px');
+
+toTopLink.appendChild(toTopArrowImage);
+document.body.appendChild(toTopLink);
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    toTopLink.style.display = 'block'; // Show the link
+  } else {
+    toTopLink.style.display = 'none'; // Hide the link
+  }
+});
